97c837f2134172b50c0e53730008ab0d46ac9950
Ramon Lodi <ramonlodi11@gmail.com>
2026-02-11 17:28:55 -0300

    Spring Boot structure and gitignore

---
+++ src/main/java/com/erpfinanceiro/ErpFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.erpfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpFinanceiroApplication.class, args);
+	}
+
+}
